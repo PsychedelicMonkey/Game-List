cf7f07cf83e0f3272df0877f4e6cc36b53dff245
Ryan <45304409+PsychedelicMonkey@users.noreply.github.com>
2023-08-09 14:42:51 -0600

    Game grid

--- resources/views/game/index.blade.php
+++ resources/views/game/index.blade.php
@@ -1,16 +1,32 @@
 @extends('layouts.app')
 
 @section('content')
-    <h1 class="text-3xl font-semibold underline">Game List</h1>
+    <div class="my-3">
+        <h1 class="text-3xl font-semibold underline">Game List</h1>
+    </div>
 
     @if(count($games) > 0)
-        <div>
+        <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 mt-4">
             @foreach($games as $game)
-                <div>
-                    <img src="{{ $game->images()->first()->image['url'] }}" alt="" />
-                    <h3>{{ $game->title }}</h3>
-                    <a href="{{ route('game-list.show', $game->slug) }}">Read</a>
-                </div>
+                <a
+                    href="{{ route('game-list.show', $game) }}"
+                    class="group inline-block border-2 no-underline text-black duration-200 hover:border-purple-600"
+                >
+                    <span class="flex flex-col">
+                        <img
+                            src="{{ $game->images()->first()->image['url'] }}"
+                            alt=""
+                            class="w-full h-48 object-cover"
+                            loading="lazy"
+                        />
+
+                        <span class="flex-1 px-3 py-4 duration-200 group-hover:bg-purple-600">
+                            <h3 class="text-lg font-semibold duration-200 group-hover:text-white">{{ $game->title }}</h3>
+                            <h4 class="duration-200 group-hover:text-white">{{ $game->genre->name }}</h4>
+                            <h5 class="duration-200 group-hover:text-white">{{ date('Y', strtotime($game->release_date)) }}</h5>
+                        </span>
+                    </span>
+                </a>
             @endforeach
         </div>
     @else
