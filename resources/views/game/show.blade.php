d14206fb32823831a29a14dae6acd7d5c13153f1
Ryan <45304409+PsychedelicMonkey@users.noreply.github.com>
2023-08-09 19:29:42 -0600

    Game show page facelift

--- resources/views/game/show.blade.php
+++ resources/views/game/show.blade.php
@@ -1,12 +1,36 @@
 @extends('layouts.app')
 
 @section('content')
-    <img src="{{ $game->images()->first()->image['url'] }}" alt="" />
-    <h1 class="text-3xl font-semibold underline">{{ $game->title }}</h1>
-    <h3>{{ $game->developer->name }}</h3>
-    <h3>{{ $game->genre->name }}</h3>
+    @auth
+        <div class="mt-3">
+            @can('update', $game)
+                <div class="flex justify-center gap-2 mb-3">
+                    <a href="{{ route('game-list.edit', $game) }}"
+                       class="inline-block w-14 border-2 hover:bg-transparent text-white text-center no-underline transition-colors duration-200 rounded-md px-2 py-1
+                                border-blue-600 bg-blue-600 hover:text-blue-600"
+                    >
+                        Edit
+                    </a>
+                </div>
+            @endcan
+        </div>
+    @endauth
 
-    @can('update', $game)
-        <a href="{{ route('game-list.edit', $game) }}">Edit this game</a>
-    @endcan
+    <div class="flex justify-between items-start my-3">
+        <div class="w-full md:w-2/3 m-auto">
+            <img src="{{ $game->images()->first()->image['url'] }}" alt="" class="w-full m-auto" />
+
+            <div class="my-4 pb-2 border-b-2 border-purple-300">
+                <h1 class="text-3xl font-bold mb-2 lg:text-4xl">{{ $game->title }}</h1>
+                <h3 class="text-xl font-semibold text-purple-900">{{ $game->developer->name }}</h3>
+            </div>
+
+            <div>
+                <h4 class="text-lg">Genre: <strong>{{ $game->genre->name }}</strong></h4>
+                <h4 class="text-lg">
+                    Release Date: <strong>{{ date('F j, Y', strtotime($game->release_date)) }}</strong>
+                </h4>
+            </div>
+        </div>
+    </div>
 @endsection
