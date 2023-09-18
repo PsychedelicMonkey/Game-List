0ee7a7a886c08f650bc492889141fbce21f931b1
Ryan <45304409+PsychedelicMonkey@users.noreply.github.com>
2023-09-18 13:54:36 -0600

    Metadata tags

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,6 +5,23 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    {{-- TODO: Change the description --}}
+
+    <!-- Primary Meta Tags -->
+    <meta property="title" content="{{ !empty($title) ? $title : 'Game List' }}">
+    <meta property="description" content="This is the Game List website">
+
+    <!-- Open Graph / Facebook -->
+    <meta property="og:title" content="{{ !empty($title) ? $title : 'Game List'}}">
+    <meta property="og:type" content="website">
+    <meta property="og:url" content="{{ url()->current() }}">
+    <meta property="og:description" content="This is the Game List website">
+
+    <!-- Twitter -->
+    <meta property="twitter:card" content="summary">
+    <meta property="twitter:title" content="{{ !empty($title) ? $title : 'GameList' }}">
+    <meta property="twitter:description" content="This is the Game List website">
+    <meta property="twitter:url" content="{{ url()->current() }}">
 
     @empty($title)
         <title>Game List</title>
